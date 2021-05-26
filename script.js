d9d488f89abad8707c1a6be5d86ebdab314c0a45
ash12-coder <ashish.agrawal_cs18@gla.ac.in>
2021-05-26 18:59:37 +0800

    3 files modified

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onscroll = function() {
   if (document.body.scrollTop > 0) {
-    document.getElementById('navbar').style.background = '#9c1de7';
+    document.getElementById('navbar').style.background = 'linear-gradient(to right, rgb(195, 20, 50), rgb(36, 11, 54))';
+
   } else {
-    document.getElementById('navbar').style.background = '#581b98';
+    document.getElementById('navbar').style.background = 'linear-gradient(to right, rgb(237, 33, 58), rgb(147, 41, 30))';
   }
 };
